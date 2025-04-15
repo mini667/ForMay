6fafb3f4c9c2eb66c42c24c5a756f25471b3442a
mini667 <mrerbol08@gmail.com>
2025-04-15 16:35:35 +0600

    Adding third character contoller

---
+++ Assets/Scripts/MenuScript.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MenuScript : MonoBehaviour
+{
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+}
